b22e0c2c29a4843d0a789ae5c61f3a4cbe2764e5
Suyog Garg <suyog999sg@gmail.com>
2023-02-13 11:52:53 +0900

    correct doc error and make sphinx docs the error was due to abs and rel module imports and is resolved by adding the source directory to the python path in init.py Remember while building the Sphinx doc, the path to source directory needs to be entered in the conf.py after sphinx-quickstart is run and before executing sphinx-apidoc, so that the correct source modules are located and the doctree made

--- new-docs/_build/html/_sources/modules.rst.txt
+++
@@ -1,7 +0,0 @@
-xmmPipeline
-===========
-
-.. toctree::
-   :maxdepth: 4
-
-   xmmPipeline
--- src/xmmPipeline/__init__.py
+++ src/xmmPipeline/__init__.py
@@ -1,11 +1,23 @@
 
-from . import *
-from .epicObj import *
-from .epicPipeline import *
-from .epicPileup import *
-from .epicSpectra import *
+import os
+import sys
 
-from .plotAnal import *
-from .convert import *
+# Add directory to path so that Python can find it.
+# This also resolves the "module not found" error
+# during import from outside the directory (e.g. in Sphinx-autodoc)
+# and the error in absolute imports among scripts within directory.
 
-__all__ = ['xmmPipeline']
+CURR_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(CURR_DIR)
+
+#from . import *
+from epicObj import *
+from epicPipeline import *
+from epicPileup import *
+from epicSpectra import *
+
+from findOverlap import *
+from plotAnal import *
+from convert import *
+
+#__all__ = ['xmmPipeline']
